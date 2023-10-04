3a8497c9d4122091bfdbd9459df560de84e57f05
gabrielepetralia <gabri.petralia03@gmail.com>
2023-10-04 18:05:50 +0200

    add style

--- src/main/java/org/java/app/SpringLaMiaPizzeriaCrudApplication.java
+++ src/main/java/org/java/app/SpringLaMiaPizzeriaCrudApplication.java
@@ -20,11 +20,11 @@ public class SpringLaMiaPizzeriaCrudApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
-		Pizza pizza1 = new Pizza("margherita", "pizza con pomodoro e mozzarella", "https://www.unmondodisapori.it/wp-content/uploads/2017/10/margherita.jpg", 5.50);
-		Pizza pizza2 = new Pizza("tedesca", "pizza con wurstel e patatine", "https://www.scattidigusto.it/wp-content/uploads/2015/11/pizza-wurstel-patatine-pomodoro.jpg", 6.50);
-		Pizza pizza3 = new Pizza("norma", "pizza con melanzane e ricotta", "https://cdn.cook.stbm.it/thumbnails/ricette/144/144902/hd750x421.jpg", 7.50);
-		Pizza pizza4 = new Pizza("tonnara", "pizza con tonno e cipolla","https://www.gustissimo.it/articoli/ricette/pizze/pizza-tonno-e-cipolla.jpg", 7.00);
-		Pizza pizza5 = new Pizza("diavola", "pizza con salame piccante","https://www.saporidellamurgia.com/prodotti/0302628/pizza_calabrese.jpg", 7.00);
+		Pizza pizza1 = new Pizza("Margherita", "pizza con pomodoro e mozzarella", "https://www.unmondodisapori.it/wp-content/uploads/2017/10/margherita.jpg", 5.50);
+		Pizza pizza2 = new Pizza("Tedesca", "pizza con wurstel e patatine", "https://www.scattidigusto.it/wp-content/uploads/2015/11/pizza-wurstel-patatine-pomodoro.jpg", 6.50);
+		Pizza pizza3 = new Pizza("Norma", "pizza con melanzane e ricotta", "https://cdn.cook.stbm.it/thumbnails/ricette/144/144902/hd750x421.jpg", 7.50);
+		Pizza pizza4 = new Pizza("Tonnara", "pizza con tonno e cipolla","https://www.gustissimo.it/articoli/ricette/pizze/pizza-tonno-e-cipolla.jpg", 7.00);
+		Pizza pizza5 = new Pizza("Diavola", "pizza con salame piccante","https://www.saporidellamurgia.com/prodotti/0302628/pizza_calabrese.jpg", 7.00);
 		
 		pizzaService.save(pizza1);
 		pizzaService.save(pizza2);
